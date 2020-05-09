ad6f8a5e3e31351b8d942ae4b5a9b86b0b5fc663
Cedricium <amaya.cedric@gmail.com>
2020-05-08 20:19:52 -0700

    Complete files 12—14

--- src/13_file_io.py
+++ src/13_file_io.py
@@ -5,15 +5,30 @@ at how to read and write to files here:
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
 
+from pathlib import Path
+current_directory = Path(__file__).resolve().parent
+
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 # Note: pay close attention to your current directory when trying to open "foo.txt"
+with open('%s/foo.txt' % current_directory) as f:
+    lines = f.readlines()
+    print(*(line for line in lines))
 
-# YOUR CODE HERE
+f.close()
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
 # then close the file. Open up "bar.txt" and inspect it to make
 # sure that it contains what you expect it to contain
 
-# YOUR CODE HERE
+lines = [
+    'This is the first line,\n',
+    'and this is the second line.\n',
+    'Finally, this is the third line!',
+]
+
+with open('%s/boo.txt' % current_directory, 'w') as f:
+    f.writelines(lines)
+
+f.close()
